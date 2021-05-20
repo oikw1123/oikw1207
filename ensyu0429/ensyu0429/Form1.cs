15913edda86a03ae68f4b7b8df6a75edbd061054
irishu <iri18shu@gmail.com>
2021-05-20 09:17:53 +0900

    Initial commit

---
+++ ensyu0429/ensyu0429/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ensyu0429
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //　button1の処理
+            MessageBox.Show(" こんにちは　私の名前は及川颯大です");
+        }
+    }
+}
